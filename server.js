7ee87bdf3db2127fbfe9ced0f7616b73bc5a8bad
Tyler Johnson <tyler@csdconsultants.us>
2021-07-06 16:03:22 -0700

    updated with a route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Connected using Elastic Beanstalk');
+});
+
 app.listen(PORT, () => {
   console.log('server is up on port ' + PORT);
 });
